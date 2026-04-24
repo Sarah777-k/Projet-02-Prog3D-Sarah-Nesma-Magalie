9a0d9ab8e6b0e1176608cd28198f7fba6f8fe3b7
Magalie Abada <magalie.abada@gmail.com>
2026-04-24 17:24:34 -0400

    Head Comment

--- js/utils/UtilsAleatoire.js
+++ js/utils/UtilsAleatoire.js
@@ -1,5 +1,5 @@
 /*
     Fichier: UtilsAleatoire.js
     Nom:  Magalie Abada
-    But: Librairie de fonctions utilitaires
+    But: Librairie de fonctions utilitaires pour le placement aléatoire des objets
 */

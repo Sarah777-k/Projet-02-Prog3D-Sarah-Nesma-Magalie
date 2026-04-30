fce5a61d8d2c112f1f9fdf74abd9014225a4e34a
Sarah777-k <sarahkhodjaoui5@gmail.com>
2026-04-29 22:10:01 -0400

    ajout texture

--- js/graphique/MaterialFactory.js
+++ js/graphique/MaterialFactory.js
@@ -166,8 +166,10 @@ function appliquerMateriau(gl, shaderProgram, materiau) {
 // ============================================================
 function initTextures(gl) {
     textureMurOuvrable = chargerTexture(gl, "../assets/textures/mur_ouvert.png");
+    //textureMurOuvrable = chargerTexture(gl, "../assets/backrooms/backrooms-wall-diffuse.png");
     textureMurSolide   = chargerTexture(gl, "../assets/textures/mur_solide.png");
     texturePlancher    = chargerTexture(gl, "../assets/textures/plancher.png");
+    //texturePlancher    = chargerTexture(gl, "../assets/backrooms/backrooms-carpet-diffuse.png");
     texturePlafond     = chargerTexture(gl, "../assets/textures/plafond.png");
     textureEnclos      = chargerTexture(gl, "../assets/textures/mur_ouvert.png");
     textureFleche      = chargerTexture(gl, "../assets/textures/fleche.jpg");
@@ -197,6 +199,15 @@ function chargerTexture(gl, cheminImage) {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+        //pour les textures avec des dimensions non puissances de 2, on doit utiliser CLAMP_TO_EDGE et pas REPEAT
+            if ((image.width & (image.width - 1)) === 0 && (image.height & (image.height - 1)) === 0) {
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+            } else {
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            }   
     };
     image.src = cheminImage;
 

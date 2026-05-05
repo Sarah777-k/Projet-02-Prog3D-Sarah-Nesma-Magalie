34e71072a62ed04eab3687e69828f734ad1f3a1e
Magalie Abada <magalie.abada@gmail.com>
2026-05-04 20:15:25 -0400

    Delete 2 empty files

--- js/core/renderer.js
+++
@@ -1,5 +0,0 @@
-/*
-    Fichier: renderer.js
-    Nom: Sarah Khodjaoui
-    But: Rendu 3D
-*/
--- js/utils/UtilsMath.js
+++
@@ -1,5 +0,0 @@
-/*
-    Fichier: UtilsMaths.js
-    Nom:  Sarah Khodjaoui & Magalie Abada
-    But: Librairie de fonctions utilitaires de calcul / maths
-*/

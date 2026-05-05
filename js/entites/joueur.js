773b306da00367089d66ab1bd4bfb48ccd62283c
Magalie Abada <magalie.abada@gmail.com>
2026-05-04 20:53:20 -0400

    correction ouverture mur car ouvrait murs solides

--- js/entites/joueur.js
+++ js/entites/joueur.js
@@ -243,6 +243,10 @@ function ouvrirMurDevantJoueur() {
 
     let mur3D = trouverMur3D(celluleDevant.ligne, celluleDevant.colonne);
 
+    if (mur3D.type !== TYPE_MUR_OUV) {
+        return murOuvert;
+    }
+
     if (mur3D.enOuverture == true) {
         return murOuvert;
     }
